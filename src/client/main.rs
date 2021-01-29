ea24d856c4692ce6f201176914aea763c9d54f9f
Gaukas Wang <gaukaswang@gmail.com>
2021-01-28 22:47:01 -0700

    stage

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,8 +1,12 @@
 extern crate winreg;
+extern crate native_windows_gui as nwg;
+extern crate native_windows_derive as nwd;  // Optional. Only if the derive macro is used.
 use std::io;
 use win_protocol_handler::*;
 
 fn main() -> io::Result<()> {
+    
+
     let mut list_entries = Vec::<(String, String, String)>::new();
     
     scan_protocol(&mut list_entries, true);
